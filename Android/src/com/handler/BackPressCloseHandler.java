11a3856bc201ac1b661069c96cd68dcbffeee188
SoonMyeong <31875043+SoonMyeong@users.noreply.github.com>
2017-09-12 13:48:20 +0900

    Create BackPressCloseHandler.java

---
+++ Android/src/com/handler/BackPressCloseHandler.java
@@ -0,0 +1,35 @@
+package com.handler;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class BackPressCloseHandler {
+	private long backKeyPressedTime = 0;
+	private Toast toast;
+
+	private Activity activity;
+
+	public BackPressCloseHandler(Activity context) {
+		this.activity = context;
+	}
+
+	public void onBackPressed() {
+		if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+			backKeyPressedTime = System.currentTimeMillis();
+			showGuide();
+			return;
+		}
+		if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+			activity.finish();
+			toast.cancel();
+		}
+	}
+
+
+	private void showGuide() {
+		toast = Toast.makeText(activity, "\'\'.",
+				Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+}

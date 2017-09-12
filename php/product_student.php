a2ec2d47c9efcb47ed3547bac243b8ba5dd2755f
SoonMyeong <31875043+SoonMyeong@users.noreply.github.com>
2017-09-12 13:19:10 +0900

    Create product_student.php

---
+++ php/product_student.php
@@ -0,0 +1,16 @@
+[{
+        "food_name": "김밥",
+        "food_image": "http:///food/4000won/gimbab.jpg",
+        "price": 1500
+    },
+    {
+        "food_name": "라면",
+        "food_image": "http:///food/4000won/ramyen.jpg",
+        "price": 2500
+    },
+    {
+        "food_name": "떡볶이",
+        "food_image": "http:///food/5000won/dduck.jpg",
+        "price": 3000
+    }
+    ]

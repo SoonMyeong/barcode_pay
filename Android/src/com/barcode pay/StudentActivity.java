c868ee9902a9d6ed766eabe2ab4c61384226afc7
SoonMyeong <31875043+SoonMyeong@users.noreply.github.com>
2017-09-12 14:01:05 +0900

    Create StudentActivity.java

---
+++ Android/src/com/barcode pay/StudentActivity.java
@@ -0,0 +1,92 @@
+package com.example.nam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import product_model.product;
+
+import com.adapters.CustomGridAdapter;
+import com.adapters.viewpageradapter_chaeum;
+import com.adapters.viewpageradapter_student;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.controller.AppController;
+import com.handler.BackPressCloseHandler;
+import com.selected.food_detail;
+import com.slidingtabs.SlidingTabLayout;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class StudentActivity extends ActionBarActivity {
+
+	ViewPager pager;
+    viewpageradapter_student adapter; 
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"«—Ωƒ","¡ﬂΩƒ","¿œΩƒ","±‚≈∏"};
+    int Numboftabs =4;
+ 
+
+	
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_food_sytle);
+  
+	
+		Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+		setSupportActionBar(toolbar);
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+				
+			}
+		});
+		
+		 // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new viewpageradapter_student(getSupportFragmentManager(),Titles,Numboftabs);
+ 
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+ 
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+ 
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+ 
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+        }
+	}
+

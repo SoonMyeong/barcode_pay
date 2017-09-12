d4267cdfd4648abecf726320829f0f89c44c1a41
SoonMyeong <31875043+SoonMyeong@users.noreply.github.com>
2017-09-12 13:45:19 +0900

    Create LruBitmapCache.java

---
+++ Android/src/com/controller/LruBitmapCache.java
@@ -0,0 +1,44 @@
+package com.controller;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+
+public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageCache {
+	
+	public static int getDefaultLruCacheSize() {
+		final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+		final int cacheSize = maxMemory /8;
+		
+		return cacheSize;
+	}
+	
+	public LruBitmapCache() {
+		this(getDefaultLruCacheSize());
+	}
+	
+	public LruBitmapCache(int sizeInKiloBytes) {
+		super(sizeInKiloBytes);
+	}
+	
+	protected int sizeOf(String key, Bitmap value) {
+		return value.getRowBytes() * value.getHeight() /1024;
+	}
+
+	@Override
+	public Bitmap getBitmap(String url) {
+		// TODO Auto-generated method stub
+		return get(url);
+	}
+
+	@Override
+	public void putBitmap(String url, Bitmap bitmap) {
+		// TODO Auto-generated method stub
+		put(url,bitmap);
+	}
+	
+	
+	
+
+}

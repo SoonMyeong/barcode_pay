539978da96b97dfafccdc0fe3a93b6265d5ed542
SoonMyeong <31875043+SoonMyeong@users.noreply.github.com>
2017-09-12 13:20:53 +0900

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -3,7 +3,7 @@ class DB_connect {
 	private $conn;
 
 public function connect() {
-	$this->conn = new mysqli("localhost", "root", "root", "android_api");
+	$this->conn = new mysqli("localhost", "root", "root", "barcode_pay");
 	
 	return $this->conn;
 	}
